5ac83e9c521cde8720f898bee1898268ed08931a
Colin Turner <colinturner34@gmail.com>
2017-05-23 10:41:28 +0100

    Flexbox styling

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,76 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { View, Text, AppRegistry, StyleSheet } from 'react-native';
+// var DayItem = require('./src/day-item');
+// var Moment = require('moment');
 
-export default class stopwatch extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
+
+var StopWatch = React.createClass({
+  render: function() {
+    return <View style={styles.container}>
+      <View style={[styles.header, this.border('yellow')]}>
+        <View style={[styles.timerWrapper, this.border('red')]}>
+          <Text>
+            00:00.00
+          </Text>
+        </View>
+        <View style={[styles.buttonWrapper, this.border('green')]}>
+          {this.startStopButton()}
+          {this.lapButton()}
+        </View>
+      </View>
+
+      <View style={[styles.footer, this.border('blue')]}>
+        <Text>
+          I am a list of Laps
         </Text>
       </View>
-    );
-  }
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    </View>
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  startStopButton: function() {
+    return <View>
+      <Text>
+        Start
+      </Text>
+    </View>
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  lapButton: function() {
+    return <View>
+      <Text>
+        Lap
+      </Text>
+    </View>
   },
+  border: function(color) {
+    return {
+      borderColor: color,
+      borderWidth: 4
+    }
+  }
 });
 
-AppRegistry.registerComponent('stopwatch', () => stopwatch);
+var styles = StyleSheet.create({
+  container: {
+    flex: 1, // Fill the entire screen
+    alignItems: 'stretch'
+  },
+  header: { // Yellow
+    flex: 1
+  },
+  footer: { // Blue
+    flex: 1
+  },
+  timerWrapper: { // Red
+    flex: 5,  // takes up 5/8ths of the available space
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonWrapper: { // Green
+    flex: 3,  // takes up 3/8ths of the available space
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  }
+});
+
+AppRegistry.registerComponent('stopwatch', () => StopWatch);
